b649205fea0182c46afa25d184bf6ba554ad8f16
sylvio-cavalcanti <89213702+sylvio-cavalcanti@users.noreply.github.com>
2021-09-16 14:52:23 -0300

    Add files via upload

---
+++ Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="results" >
+        <h1>{{ rank }}</h1>
+        <p>Reaction time: {{ score }} ms</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['score'], 
+    data(){
+        return {
+            rank: null 
+        }
+    },
+    mounted(){
+        if (this.score <= 250){
+            this.rank = "Awesome! You are a fast one!"
+        }
+        else if (this.score < 400){
+            this.rank = "Not bad, but you could be quicker!"
+        }
+        else{
+            this.rank = "Well that took a while..."
+        }
+    }
+}
+</script>
+<style>
+    .results{
+        width: 500px;
+        border-radius: 10px;
+        background: #0f7caf;
+        color: white;
+        text-align: center;
+        padding: 5px 0;
+        margin: 40px auto;
+        font-weight: 600;
+    }
+</style>
